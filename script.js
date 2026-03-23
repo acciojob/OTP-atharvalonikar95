6236f2f2905b6de8696705f4d0119b72fc2542c6
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-23 16:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const fields = document.querySelectorAll('.code')
+
+fields[0].focus();
+
+fields.forEach((field,index)=>{
+	field.addEventListener('input',(e)=>{
+		const val = e.target.value;
+		if(val.length>1){
+			field.value=val.slice(0,1)
+		}
+		if(val && index<fields.length-1){
+			fields[index+1].focus()
+		}
+	})
+	field.addEventListener('keydown',(e)=>{
+		if(e.key==="Backspace"){
+			if(!field.value &&index>0){
+				fields[index-1].focus();
+			}
+		}
+	}
+)
+})
+
+
+
